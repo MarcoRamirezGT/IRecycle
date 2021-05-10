38abcfef08f15cc4a72b8ce2d28e35eb1d36fa73
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-05-09 19:51:48 -0600

    Initial commit

---
+++ app/src/main/java/com/example/irecycle/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.irecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_home);
+    }
+    public void onProfileClick(View view){
+        startActivity(new Intent(this,ProfileActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+    }
+}
