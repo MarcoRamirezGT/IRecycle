05f53d0cf17f9534ab230adb25c803e33245a3bc
EdmanUVG <54090748+EdmanUVG@users.noreply.github.com>
2021-05-22 11:03:09 -0600

    Pequeños cambios

--- app/src/main/java/com/example/irecycle/MaterialesActivity.java
+++ app/src/main/java/com/example/irecycle/MaterialesActivity.java
@@ -12,10 +12,6 @@ public class MaterialesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_materiales);
     }
     public void onBackHome(View view){
--- app/src/main/java/com/example/irecycle/SabiasQueActivity.java
+++ app/src/main/java/com/example/irecycle/SabiasQueActivity.java
@@ -12,9 +12,6 @@ public class SabiasQueActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
         setContentView(R.layout.activity_sabias_que);
     }
     public void onBackHomeSB(View view){
